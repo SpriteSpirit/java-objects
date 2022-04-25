29608d2437104fd1f536f6db6f9f6deaccde41b8
Sprite_Spirit <a.backer@yandex.com>
2022-04-25 21:24:34 +0300

    init

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,19 @@ public class Main {
         int ageJohn = 13;
 
         String nameSarah = "Sarah";
+        int ageSarah = 30;
+
+        String[] names = {"John", "Sarah"};
+        int[] ages = {13, 30};
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println("Name - " + names[i] + ". Age - " + ages[i]);
+        }
+        Person sarah = new Person();
+        sarah.name = "Sarah";
+        System.out.println("Sarah's name: " + sarah.name);
+        sarah.age = 30;
+        System.out.println("Sarah's age: " + sarah.age);
 
     }
 }
