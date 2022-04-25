2d4ef5eeef8c12baa3847e4298838b89d6411ac5
Sprite_Spirit <a.backer@yandex.com>
2022-04-25 21:16:51 +0300

    init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String nameJohn = "John";
+        int ageJohn = 13;
+
+        String nameSarah = "Sarah";
+
     }
 }
